95827ed4d6eb9bafb793f262f9ccd4d766af3283
Jasmine Rai <165194480+Assistantjasmine282@users.noreply.github.com>
2026-01-20 13:18:57 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -18,8 +18,53 @@ undefine =>  not assigned value
 symbol = > unique 
 
 */
+  /*
+1.object
+null is a object but undefined is undefined 
+console.log(typeof null)
+*/
 
-//object
-//null is a object but undefined is undefined 
-//console.log(typeof null)
+let jasmine={
+    type:"company",
+    location :"noida"
+}
+console.log(jasmine.type)
+
+
+
+//2. Arrays
+let a1 = [1, 2, 3, 4, 5];
+console.log(a1);
+
+let a2 = [1, "two", { name: "Object" }, [3, 4, 5]];
+console.log(a2);
+
+
+//3.function
+// Defining a function to greet a user
+function greet(name) {
+     return "Hello, " + name + "!"; 
+    }
+// Calling the function
+console.log(greet("Ajay"));
+
+//4.date Object
+// Creating a new Date object for the
+// current date and time
+let currentDate = new Date();
+
+// Displaying the current date and time
+console.log(currentDate);
+
+//5. regular expression 
+// Creating a regular expression to match the word "hello"
+let pattern = /hello/;
+
+// Testing the pattern against a string
+// Returns false because "hello" is not present
+let result = pattern.test("Hello, world!"); 
+
+console.log(result);
+
+
 
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score ="33abc"
+let value = null;
+let data = undefined;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+//conversion
+let scoreInNumber = Number(score); // this is used to convert string in number 
+let valueInNumber = Number(value);
+let  dataInNumber = Number(data)
+console.log(typeof scoreInNumber);  
+console.log(scoreInNumber);  //output NaN not a number   
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ console.log(typeof dataInNumber);
+ console.log(dataInNumber);
+ 
