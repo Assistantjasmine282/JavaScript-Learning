e300362e9868efe5d45272acbc849bb85feb714e
Jasmine Rai <165194480+Assistantjasmine282@users.noreply.github.com>
2026-01-20 12:40:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)     we are using nodejs not browser so this create error 
+
+
+let name="jasmine"
+let age=22
+let isLoggedIn=false
+let state= null
+
+/*premitive datatype 
+number => 2 to 53
+bigint
+string
+boolean => false/true
+null => standalone value  or empty value
+undefine =>  not assigned value
+symbol = > unique 
+
+*/
+
+//object
+//null is a object but undefined is undefined 
+//console.log(typeof null)
+
