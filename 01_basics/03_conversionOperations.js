ab104de1aac4203f55dbbf35425cbfc3f6bac437
Jasmine Rai <165194480+Assistantjasmine282@users.noreply.github.com>
2026-01-22 05:16:48 +0000

    conversion of datatype

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,5 @@
+
+
 let score ="33abc"
 let value = null;
 let data = undefined;
@@ -5,7 +7,9 @@ let data = undefined;
 console.log(typeof score);
 console.log(typeof(score));
 
+
 //conversion
+
 let scoreInNumber = Number(score); // this is used to convert string in number 
 let valueInNumber = Number(value);
 let  dataInNumber = Number(data)
@@ -17,3 +21,26 @@ console.log(scoreInNumber);  //output NaN not a number
  console.log(typeof dataInNumber);
  console.log(dataInNumber);
  
+
+
+ //"33" => 33
+ //"33abc" => NaN
+ //true => 1,false=> 0
+
+
+ let isLoggedIn =1;
+ let booleanIsLoggedIn =Boolean(isLoggedIn)
+ console.log(valueInNumber);     //output -> true
+
+
+ //in boolean conversion 
+ // 1 => true;
+// 0 => false 
+//""  => false
+// "hitesh"  => true
+
+let someNumber =33
+let stringNumber= String(someNumber)
+
+console.log(stringNumber);     //33
+console.log(typeof stringNumber);  //string
