6b8ff09fb1838f8aca7fca8b1af60c0866af72c0
Jasmine Rai <165194480+Assistantjasmine282@users.noreply.github.com>
2026-01-22 06:20:24 +0000

    stack and heap memory allocation

---
+++ 01_basics/05_stackHeap.js
@@ -0,0 +1,15 @@
+let myYoutubename= "jasmineRai"
+
+let anothername= myYoutubename
+anothername= "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let user1 ={
+    email:"jasminerai@gmail.com",
+    upi:"user@ybl"
+}
+
+let user2 =user1
+user2.email="jamsine12@gmail.com"
